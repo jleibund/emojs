b9a1a7f3d8e1090ff01964c1a990b0202e06f7a0
jleibund <jleibund@gmail.com>
2012-12-28 19:11:01 -0500

    - trying different libuv setup to work around issue with appjs hogging the default loop.

--- NodeEPOCDriver.cc
+++ NodeEPOCDriver.cc
@@ -36,12 +36,13 @@ private:
   unsigned int userID;
   int run;
   char * profile;
+  uv_async_t async;
   uv_loop_t* loop;
   uv_loop_t* work;
   uv_thread_t thread;
   uv_timer_t timer;
   uv_idle_t idler;
-  uv_async_t g_async;
+  //uv_async_t g_async;
   Persistent<Function> cb;
 public:
 
@@ -175,11 +176,16 @@ public:
 //                uv_thread_create(&hw->thread, timer_cb, &hw);
                 hw->run=1;
 
-                cout << "before work" <<endl;
+//                cout << "before work" <<endl;
+
+//                uv_work_t *req3 = new uv_work_t();
+//                req3->data = hw;
+//                uv_queue_work(hw->work, req3, runwork, process);
+
                 int status = uv_queue_work(hw->loop, req, timer_cb, after_timer);
                 assert(status == 0);
 
-                cout << "after work" <<endl;
+                cout << "starting epoc event loop" <<endl;
 
 //                uv_idle_init(hw->loop, &hw->idler);
 //                uv_idle_start(&hw->idler, &hw->timer_cb);
@@ -205,7 +211,7 @@ public:
 //        NodeEPOCDriver* hw = static_cast<NodeEPOCDriver*>(timer->data);
 
 //        if (hw->run ==1){
-
+        while (true){
             if (hw->connected == 1){
                 EmoEngineEventHandle eEvent = EE_EmoEngineEventCreate();
                 EmoStateHandle eState = EE_EmoStateCreate();
@@ -217,7 +223,7 @@ public:
                     if (eventType == EE_EmoStateUpdated){
                         EE_EmoEngineEventGetEmoState(eEvent, eState);
 
-                cout << "fire" <<endl;
+//                cout << "fire" <<endl;
 
                         baton_t* baton = new baton_t();
                         baton->hw = hw;
@@ -255,28 +261,39 @@ public:
                         // create an event to throw on queue for processing..
                         uv_work_t *req2 = new uv_work_t();
                         req2->data = baton;
+
                         int status = uv_queue_work(hw->loop, req2, process, after_process);
+//                        uv_async_init(hw->loop, &hw->async, after_process);
+//                        hw->async.data = hw;
+//                        uv_async_send(&hw->async);
+
+
                    //     uv_run_once(hw->work);
                         assert(status == 0);
                      }
                 }
                 EE_EmoStateFree(eState);
                 EE_EmoEngineEventFree(eEvent);
-//            }
+            }
            // hw->Ref();
 
 //sleep(1);
 
         }
     }
+
+    static void runwork(uv_work_t* req){
+        NodeEPOCDriver *hw = static_cast<NodeEPOCDriver *>(req->data);
+        uv_run(hw->work);
+    }
+
     static void after_timer(uv_work_t* req) {
   //                  cout << "after timer" <<endl;
-        NodeEPOCDriver *hw = static_cast<NodeEPOCDriver *>(req->data);
-        if (hw->run ==1){
-     //       sleep(1);
-            int status = uv_queue_work(hw->loop, req, timer_cb, after_timer);
-            assert(status == 0);
-        }
+//        NodeEPOCDriver *hw = static_cast<NodeEPOCDriver *>(req->data);
+//        if (hw->run ==1){
+//            int status = uv_queue_work(hw->loop, req, timer_cb, after_timer);
+//            assert(status == 0);
+//        }
     }
 
     static Handle<Value> disconnect(const Arguments& args){
@@ -303,58 +320,10 @@ public:
     
     static void process(uv_work_t* req)
     {
-//        baton_t *baton = static_cast<baton_t *>(req->data);
-//        NodeEPOCDriver* hw = baton->hw;
-//        baton->update = false;
-//        if (hw->connected == 1){
-//            EmoEngineEventHandle eEvent = EE_EmoEngineEventCreate();
-//            EmoStateHandle eState = EE_EmoStateCreate();
-//            int state = EE_EngineGetNextEvent(eEvent);
-//            if (state == EDK_OK) {
-//                 EE_Event_t eventType = EE_EmoEngineEventGetType(eEvent);
-//                 EE_EmoEngineEventGetUserId(eEvent, &hw->userID);
-//
-//                if (eventType == EE_EmoStateUpdated){
-//                    EE_EmoEngineEventGetEmoState(eEvent, eState);
-//                    baton->timestamp = ES_GetTimeFromStart(eState);
-//                    baton->cog = static_cast<int>(ES_CognitivGetCurrentAction(eState));
-//                    baton->cog_power = ES_CognitivGetCurrentActionPower(eState);
-//                    baton->blink = ES_ExpressivIsBlink(eState);
-//                    baton->winkLeft = ES_ExpressivIsLeftWink(eState);
-//                    baton->winkRight = ES_ExpressivIsRightWink(eState);
-//                    baton->lookLeft = ES_ExpressivIsLookingLeft(eState);
-//                    baton->lookRight = ES_ExpressivIsLookingRight(eState);
-//                    std::map<EE_ExpressivAlgo_t, float> expressivStates;
-//                    EE_ExpressivAlgo_t upperFaceAction = ES_ExpressivGetUpperFaceAction(eState);
-//                    float			   upperFacePower  = ES_ExpressivGetUpperFaceActionPower(eState);
-//                    EE_ExpressivAlgo_t lowerFaceAction = ES_ExpressivGetLowerFaceAction(eState);
-//                    float			   lowerFacePower  = ES_ExpressivGetLowerFaceActionPower(eState);
-//                    expressivStates[ upperFaceAction ] = upperFacePower;
-//                    expressivStates[ lowerFaceAction ] = lowerFacePower;
-//                    baton->eyebrow= expressivStates[ EXP_EYEBROW ];
-//                    baton->furrow= expressivStates[ EXP_FURROW ]; // furrow
-//                    baton->smile= expressivStates[ EXP_SMILE ]; // smile
-//                    baton->clench= expressivStates[ EXP_CLENCH ]; // clench
-//                    baton->smirkLeft= expressivStates[ EXP_SMIRK_LEFT  ]; // smirk left
-//                    baton->smirkRight= expressivStates[ EXP_SMIRK_RIGHT ]; // smirk right
-//                    baton->laugh= expressivStates[ EXP_LAUGH       ]; // laugh
-//
-//                    // Affectiv Suite results
-//                    baton->shortTermExcitement= ES_AffectivGetExcitementShortTermScore(eState);
-//                    baton->longTermExcitement= ES_AffectivGetExcitementLongTermScore(eState);
-//
-//                    baton->engagementOrBoredom= ES_AffectivGetEngagementBoredomScore(eState);
-//
-//                    baton->update = true;
-//                }
-//            }
-//            EE_EmoStateFree(eState);
-//            EE_EmoEngineEventFree(eEvent);
-//        }
-        // this is where we need to poll for the next event
     }
     
     static void after_process(uv_work_t* req) {
+//    static void after_process(uv_async_t* req, int status) {
         HandleScope scope;
         baton_t *baton = static_cast<baton_t *>(req->data);
      //   baton->hw->Unref();
@@ -366,7 +335,7 @@ public:
 
         if (baton->update){
 
-            cout<<"update!"<<endl;
+//            cout<<"update!"<<endl;
 
             // these will be the fields from the event
             obj->Set(String::NewSymbol("time"), Number::New(baton->timestamp));
@@ -400,7 +369,7 @@ public:
                 FatalException(try_catch);
             }
         }
-
+//        uv_close((uv_handle_t*) req, NULL);
         delete baton;
     }
 
